61bd3208f19dbd8d3bd333fc20fa57d6bdb088e8
Heitzz <dheitzz@icloud.com>
2021-09-30 21:59:29 +0300

    Init

---
+++ code/GangsGame.cs
@@ -0,0 +1,31 @@
+﻿using MinimalExample;
+using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whosyourgarry
+{
+	public partial class Game : Sandbox.Game
+	{
+		public Game()
+		{
+
+		}
+
+		/// <summary>
+		/// A client has joined the server. Make them a pawn to play with
+		/// </summary>
+		public override void ClientJoined( Client client )
+		{
+			base.ClientJoined( client );
+
+			var player = new MinimalPlayer();
+			client.Pawn = player;
+
+			player.Respawn();
+		}
+	}
+}
